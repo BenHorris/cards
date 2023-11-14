15f37c20815f10d8a059f947ffcfdef7a6f8b8a1
benha <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>
2023-11-14 15:07:42 +1030

    Scoreboard started

--- main.py
+++ main.py
@@ -5,8 +5,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    player = "John"
-    return render_template('index.html', player=player)
+    players = {
+        'Kelly Oubre Jr': {
+            'name': "Kelly Oubre Jr",
+            'points': 32,
+            'rebounds': 14,
+            'assists': 17,
+            'steals': 6,
+            'blocks': 8
+        },
+        'Lebron James': {
+            'name': "Lebron James",
+            'points': 37,
+            'rebounds': 19,
+            'assists': 11,
+            'steals': 5,
+            'blocks': 9
+        },
+        'Michael Jordan': {
+            'name': "Michael Jordan",
+            'points': 3,
+            'rebounds': 1,
+            'assists': 2,
+            'steals': 0.2,
+            'blocks': 0
+        },
+        'Lonzo Ball': {
+            'name': "Lonzo Ball",
+            'points': 42,
+            'rebounds': 16,
+            'assists': 27,
+            'steals': 12,
+            'blocks': 7
+        },
+    }
+    total_points = players["Kelly Oubre Jr"]["points"]+players["Lebron James"]["points"]+players["Michael Jordan"]["points"]+players["Lonzo Ball"]["points"]
+    total_rebounds = players["Kelly Oubre Jr"]["rebounds"] + players["Lebron James"]["rebounds"] + players["Michael Jordan"]["rebounds"] +players["Lonzo Ball"]["rebounds"]
+    scoreboard = {
+        'total_points': total_points,
+        'total_rebounds': total_rebounds,
+        'total_totals': total_points+total_rebounds
+    }
+    return render_template('index.html', players=players, scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
